dd1f05e8222bfd415270093a24979d88132d99cf
imalpha <imalpha@mailoo.org>
2016-01-02 18:45:07 +0100

    Build without fish

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-#!/usr/bin/fish
-env PKG_CONFIG_PATH="/usr/lib/pkgconfig/:/usr/local/lib/pkgconfig/" valac main.vala artworkdb.vala id3tag.vapi    --pkg libgda-6.0 --pkg libxml-2.0 --pkg gio-2.0 --pkg gee-0.8 --Xcc=-lid3tag -g
+#!/usr/bin/env bash
+#PKG_CONFIG_PATH="/usr/lib/pkgconfig/:/usr/local/lib/pkgconfig/"
+valac main.vala artworkdb.vala   --pkg libgda-6.0 --pkg libxml-2.0 --pkg gio-2.0 --pkg gee-0.8 --Xcc=-lid3tag -g
